aff67de4eee2df69be18801be83f07d60b6b036a
Solanki Bhavesh <78946856+developer-bhavesh@users.noreply.github.com>
2025-01-08 17:30:25 +0530

    Foost App

---
+++ FoostApplication/FoostApp/forgotpassword.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import SDWebImageSwiftUI
+
+
+
+struct forgotpassword: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    forgotpassword()
+}
